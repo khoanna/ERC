897299e8ddc170ca51795b4907d145feab2fd6c8
khoanna <khoa210105@gmail.com>
2025-11-30 23:24:15 +0700

    Add deployment and transaction records for SimpleAccount and EntryPoint interactions
    - Created deployment record for SimpleAccount contract with address 0xd7a66ece1719fbc6593181615bfb6d2f28664416.
    - Added transaction details for depositing to EntryPoint contract at address 0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789, including two separate deposit transactions with different values.
    - Included transaction receipts and logs for both deposit actions, confirming successful execution and gas usage.

--- src/script/deploy.s.sol
+++ src/script/deploy.s.sol
@@ -1,18 +1,46 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.23;
 
 import {Script, console} from "lib/forge-std/src/Script.sol";
-import {MyToken} from "../ERC20.sol";
+import {
+    IEntryPoint
+} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {SimpleAccount} from "../ERC4337/SimpleAccount.sol";
+import {Paymaster} from "../ERC4337/Paymaster.sol";
 
-contract ERC20Script is Script {
-    MyToken public myToken;
+contract SimpleAccountScript is Script {
+    SimpleAccount public simpleAccount;
 
     function setUp() public {}
+
+    address constant ENTRY_POINT_ADDRESS =
+        0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
+
+    address constant OWNER_ADDRESS = 0xd5de8324D526A201672B30584e495C71BeBb3e9A;
 
     function run() public {
         vm.startBroadcast();
 
-        myToken = new MyToken();
+        simpleAccount = new SimpleAccount(ENTRY_POINT_ADDRESS, OWNER_ADDRESS);
+
+        vm.stopBroadcast();
+    }
+}
+
+contract PaymasterScript is Script {
+    Paymaster public paymaster;
+
+    function setUp() public {}
+
+    address constant ENTRY_POINT_ADDRESS =
+        0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
+
+    function run() public {
+        vm.startBroadcast();
+
+        IEntryPoint(ENTRY_POINT_ADDRESS).depositTo{value: 0.1 ether}(
+            address(0xd7a66Ece1719FBC6593181615bfb6d2F28664416)
+        );
 
         vm.stopBroadcast();
     }
